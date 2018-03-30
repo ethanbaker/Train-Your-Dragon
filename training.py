467d4eceef04e869c9ddab02153caca9a630918d
Ethan Baker <32339256+ethanbaker@users.noreply.github.com>
2018-03-30 17:17:30 -0400

    Started main and ask train

--- training.py
+++ training.py
@@ -1,1 +1,10 @@
-pass
+import random as r
+from time import sleep as s
+import classes as cl
+import os as o
+
+def askTrain():
+    training = input('What would you like to train on?\n')
+
+def main():
+    askTrain()
