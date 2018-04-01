d3d5057b2ba1dc24d6934a3fadde26243a2f2ebf
Ethan Baker <32339256+ethanbaker@users.noreply.github.com>
2018-03-31 20:19:38 -0400

    Worked on games

--- training.py
+++ training.py
@@ -6,11 +6,12 @@ import random
 import time
 import save as sav
 import load as loa
+import math
 
 def askTrain():
     loa.load_game()
     loa.load_scores()
-    training = input(c.b + 'What would you like to train on?\nAttack - 1\nDefence - 2\nDodge - 3\nSpeed - 4\nStamina - 5\n >>> ')
+    training = input(co.b + 'What would you like to train on?\nAttack - 1\nDefence - 2\nDodge - 3\nSpeed - 4\nStamina - 5\n >>> ')
     if training == '1':
         c()
         attackAsk()
@@ -34,7 +35,7 @@ def askTrain():
 def attackAsk():
     tutorial = input(co.b + 'Do you want the tutorial on how to train for attack?\nYes - 1\nNo - 2\n >>> ')
     if tutorial == '1':
-        print(co.g + 'To play, press enter as soon as you see ATTACK appear on the screen. That faster you respond, the more XP you get.')
+        print(co.c + 'To play, press enter as soon as you see ATTACK appear on the screen. That faster you respond, the more XP you get.')
         input('---Press Enter to continue---')
         c()
         attackTrain()
@@ -53,31 +54,36 @@ def attackTrain():
     s(random.randint(1, 10))
     then = time.time()
     react = input('ATTACK')
+    print()
     reactionTime = time.time() - then
-    print(f"Your reaction time was {reactionTime*1000:.0f} ms")
     s(2)
-    if round(reactionTime * 1000) < cl.highScores.attack:
-        cl.highScores.attack = round(reactionTime * 1000)
-    if cl.Dragon.hap >= 30:
-        cl.Dragon.xp += 20 - round(reactionTime * 10)
-        if cl.Dragon.hap >= 80:
-            cl.Dragon.xp += 5
-    if round(reactionTime * 10) < 6 and cl.Dragon.hap >= 30:
-        cl.Dragon.att += 5 - round(reactionTime * 10)
-        print(5 - round(reactionTime * 10))
+    if reactionTime <= 0.0001:
+        c()
+        print('You failed! Try again next time.')
         s(2)
-        if cl.Dragon.hap >= 80:
-            cl.Dragon.att += 1
-    cl.Dragon.hap -= round((cl.Dragon.hap - 100) * .25)
-    if cl.Dragon.hap < 0:
-        cl.Dragon.hap = 0
+    else:
+        print(f"Your reaction time was {reactionTime*1000:.0f} ms")
+        s(2)
+        if round(reactionTime * 1000) < cl.highScores.attack:
+            cl.highScores.attack = round(reactionTime * 1000)
+        if cl.Dragon.hap >= 30:
+            cl.Dragon.xp += 20 - round(reactionTime * 10)
+            if cl.Dragon.hap >= 80:
+                cl.Dragon.xp += 5
+        if round(reactionTime * 10) < 7 and cl.Dragon.hap >= 30:
+            cl.Dragon.att += 6 - math.floor(reactionTime * 10)
+            if cl.Dragon.hap >= 80:
+                cl.Dragon.att += 1
+        cl.Dragon.hap -= round((cl.Dragon.hap - 100) * .25)
+        if cl.Dragon.hap < 0:
+            cl.Dragon.hap = 0
     sav.save_game()
     sav.save_scores() 
 
 def defenceAsk():
     tutorial = input(co.b + 'Do you want the tutorial on how to train for defence?\nYes - 1\nNo - 2\n >>> ')
     if tutorial == '1':
-        print(co.g + 'To play, press enter as soon as you see DEFEND appear on the screen. Do not press anything when ATTACK appears on the screen. That faster you respond to DEFEND, the more XP you get. You will lose XP if you respond to ATTACK.')
+        print(co.c + 'To play, press enter as soon as you see DEFEND appear on the screen. Do not press anything when ATTACK appears on the screen. That faster you respond to DEFEND, the more XP you get. You will lose XP if you respond to ATTACK.')
         input('---Press Enter to continue---')
         c()
         defenceTrain()
@@ -152,24 +158,20 @@ def defenceTrain():
             breactionTime = reactionTime
         elif reactionTime >= reactionTimeTwo:
             breactionTime = reactionTimeTwo
-        else:
-            print('Fatal Error')
     if int(round(breactionTime*1000)) <= 0:
         print('You failed! Try again next time.')
         s(2)
     else:
-        print(f"Your reaction time was {breactionTime*1000:.0f} ms")
+        print(f"Your best reaction time was {breactionTime*1000:.0f} ms")
         s(2)
         if round(breactionTime * 1000) < cl.highScores.defence:
-            cl.highScores.defence = round(reactionTime * 1000)
+            cl.highScores.defence = round(breactionTime * 1000)
         if cl.Dragon.hap >= 30:
-            cl.Dragon.xp += 20 - round(reactionTime * 10)
+            cl.Dragon.xp += 20 - round(breactionTime * 10)
             if cl.Dragon.hap >= 80:
                 cl.Dragon.xp += 5
-        if round(reactionTime * 10) < 6 and cl.Dragon.hap >= 30:
-            cl.Dragon.dfc += 5 - round(reactionTime * 10)
-            print(5 - round(reactionTime * 10))
-            s(2)
+        if round(breactionTime * 10) < 7 and cl.Dragon.hap >= 30:
+            cl.Dragon.dfc += 6 - math.floor(breactionTime * 10)
             if cl.Dragon.hap >= 80:
                 cl.Dragon.dfc += 1
     cl.Dragon.hap -= round((cl.Dragon.hap - 100) * .25)
@@ -188,7 +190,7 @@ def dodgeTrain():
 def speedAsk():
     tutorial = input(co.b + 'Do you want the tutorial on how to train for speed?\nYes - 1\nNo - 2\n >>> ')
     if tutorial == '1':
-        print(co.g + 'To play, press enter twice as soon as you see START appear on the screen. The faster you respond, the more XP you get.')
+        print(co.c + 'To play, press enter twice as soon as you see START appear on the screen. The faster you respond, the more XP you get.')
         input('---Press Enter to continue---')
         c()
         speedTrain()
@@ -217,10 +219,8 @@ def speedTrain():
         cl.Dragon.xp += 20 - round(reactionTime * 10)
         if cl.Dragon.hap >= 80:
             cl.Dragon.xp += 5
-    if round(reactionTime * 10) < 6 and cl.Dragon.hap >= 30:
-        cl.Dragon.spd += 5 - round(reactionTime * 10)
-        print(5 - round(reactionTime * 10))
-        s(2)
+    if round(reactionTime * 10) < 5 and cl.Dragon.hap >= 30:
+        cl.Dragon.spd += 4 - math.floor(reactionTime * 10)
         if cl.Dragon.hap >= 80:
             cl.Dragon.spd += 1
     cl.Dragon.hap -= round((cl.Dragon.hap - 100) * .25)
@@ -229,11 +229,10 @@ def speedTrain():
     sav.save_game()
     sav.save_scores() 
 
-#TODO for stamina game, press enter 5 times and try to be consistant??
 def staminaAsk():
-    tutorial = input(co.b + 'Do you want the tutorial on how to train for speed?\nYes - 1\nNo - 2\n >>> ')
+    tutorial = input(co.b + 'Do you want the tutorial on how to train for stamina?\nYes - 1\nNo - 2\n >>> ')
     if tutorial == '1':
-        print(co.g + 'To play, press enter three times, trying to have one second in between each press as soon as you see START appear on the screen. The closer the interval is to one second, the more XP you get.')
+        print(co.c + 'To play, press enter three times, trying to have one second in between each press as soon as you see START appear on the screen. \nThe closer the interval is to one second, the more XP you get.')
         input('---Press Enter to continue---')
         c()
         staminaTrain()
@@ -262,7 +261,8 @@ def staminaTrain():
         breactionTime = reactionTime
     else:
         breactionTime = reactionTimeTwo
-    print(f"Your best interval time was {breactionTime*1000:.0f} ms")
+    breactionTime = abs(breactionTime)
+    print(f"Your best reaction time was {breactionTime*1000:.0f} ms")
     s(2)
     if round(breactionTime * 1000) < cl.highScores.stamina:
         cl.highScores.stamina = round(breactionTime * 1000)
@@ -270,10 +270,8 @@ def staminaTrain():
         cl.Dragon.xp += 15 - round(breactionTime * 10)
         if cl.Dragon.hap >= 80:
             cl.Dragon.xp += 5
-    if round(breactionTime * 10) < 6 and cl.Dragon.hap >= 30:
-        cl.Dragon.sta += 5 - round(reactionTime * 10)
-        print(5 - round(reactionTime * 10))
-        s(2)
+    if round(breactionTime * 10) < 5 and cl.Dragon.hap >= 30:
+        cl.Dragon.sta += 4 - math.floor(reactionTime * 10)
         if cl.Dragon.hap >= 80:
             cl.Dragon.sta += 1
     cl.Dragon.hap -= round((cl.Dragon.hap - 100) * .25)
