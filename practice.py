984e92385cecdd5b46aa3aa7e2d296d0a3d892ea
Ethan Baker <32339256+ethanbaker@users.noreply.github.com>
2018-04-02 14:22:49 -0400

    Working on practice gaes

--- practice.py
+++ practice.py
@@ -9,10 +9,23 @@ def ask():
     print(co.g + 'Welcome to the practice center!')
     print()
     level = input(co.b + 'What level would you like to practice with?\nLevel One - 1\nLevel Two - 2\nLevel Three - 3\n >>> ')
+    if level == '1':
+        c()
+        lvlOne()
+    elif level == '2':
+        c()
+        lvlTwo()
+    elif level == '3':
+        c()
+        lvlTwo()
+    else:
+        print('Please answer with a 1, 2, or 3.')
+        s(2)
+        c()
 
 def lvlOne():
-
-    pass
+    
+    
 def c():
     o.system('clear')
 
