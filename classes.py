1e900c70009317e62d39a724780b166855e6c611
Ethan Baker <32339256+ethanbaker@users.noreply.github.com>
2018-04-02 14:32:52 -0400

    Added random class stats and fixed bugs

--- classes.py
+++ classes.py
@@ -1,3 +1,8 @@
+import random as r
+
+names = ['Jeremy', 'Matthew', 'John', 'Willard', 'Aaorn', 'Ashley', 'Lauren', 'Jamiese', 'Catherine', 'Caroline']
+colors = ['red', 'orange', 'yellow', 'blue', 'green', 'violet', 'cyan', 'magenta']
+
 class Base():
     htp = 100
     att = 10
@@ -15,14 +20,32 @@ class Dragon(Base):
     pass
 
 class LevelOnePractice(Base):
-    pass
+    att = r.randint(10, 17)
+    dfc = r.randint(5, 12)
+    dog = r.randint(6, 11)
+    spd = r.randint(9, 12)
+    sta = r.randint(9, 12)
+    name = r.choice(names)
+    color = r.choice(colors)
 
 class LevelTwoPractice(Base):
-    pass
+    att = r.randint(23, 32)
+    dfc = r.randint(17, 25)
+    dog = r.randint(16, 21)
+    spd = r.randint(17, 19)
+    sta = r.randint(17, 19)
+    name = r.choice(names)
+    color = r.choice(colors)
 
 class LevelThreePractice(Base):
-    pass
-
+    att = r.randint(36, 43)
+    dfc = r.randint(26, 31)
+    dog = r.randint(28, 34)
+    spd = r.randint(26, 30)
+    sta = r.randint(26, 30)
+    name = r.choice(names)
+    color = r.choice(colors)
+    
 class highScores():
         attack = 10000
         defense = 10000
@@ -33,4 +56,4 @@ class highScores():
 
 
 if __name__ == '__main__':
-    print(str(Player.htp))
+    print(str(LevelThreePractice.att))
