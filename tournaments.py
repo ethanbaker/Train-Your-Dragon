b832bd4ce6e8d481c2bb984426bda0971aa34af2
Ethan Baker <32339256+ethanbaker@users.noreply.github.com>
2018-04-03 19:26:36 -0400

    Working on tournament

--- tournaments.py
+++ tournaments.py
@@ -7,7 +7,45 @@ def ask():
     tourney = input(co.b + 'What tournament would you like to enter?\nBronze - 1\nSilver - 2\nGold - 3\nCrystal - 4\n >>> ')
     if tourney == '1':
         bronzeCup()
-    elif tourney
+    elif tourney == '2':
+        silverCup()
+    elif tourney == '3':
+        goldCup()
+    elif tourney == '4':
+        crystalCup()
+    else:
+        print(co.y + 'Please answer with a 1, 2, 3, or 4.')
+        s(2)
+        c()
+        ask()
+
+def bronzeCup():
+    b.run('bronze')
+    if b.Enemy.win == False:
+        win()
+    else:
+        lose()
+
+def silverCup():
+    b.run('silver')
+    if b.Enemy.win == False:
+        win()
+    else:
+        lose()
+
+def goldCup():
+    b.run('gold')
+    if b.Enemy.win == False:
+        win()
+    else:
+        lose()
+
+def crystalCup():
+    b.run('crystal')
+    if b.Enemy.win == False:
+        win()
+    else:
+        lose()
 
 def c():
     o.system('clear')
