756f08fa0c1c4ad9210372591dfc60e0941a16ac
Ethan Baker <32339256+ethanbaker@users.noreply.github.com>
2018-04-04 19:38:54 -0400

    Little fixes on play

--- play.py
+++ play.py
@@ -5,8 +5,11 @@ import battleengine as b
 import random as r
 import colors as co
 import classes as cl
+import load
+import save
 
 def main():
+    load.load_game()
     print(co.g + 'You play with ' + cl.Dragon.name + '.')
     print(b.playerColorDef())
     s(1.5)
@@ -14,6 +17,9 @@ def main():
     cl.Dragon.hap += 45
     cl.Dragon.hap += cl.Dragon.lvl * 5
     cl.Dragon.hap += r.randint(1, 15)
+    if cl.Dragon.hap > 100:
+        cl.Dragon.hap = 100
+    save.save_game()
     c()
 
 def c():
